c17e49503c695d0eed1b4abacde9742be33456de
Dvora Gvili <dvora.gvili@gmail.com>
2024-02-20 09:56:53 +0100

    fetch data server side only

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,4 +1,10 @@
-const DrinksPage = () => {
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+
+const DrinksPage = async () => {
+  const response = await fetch(url);
+  const drinks = await response.json();
+  console.log(drinks);
   return (
     <div>
         <h1>Drinks Page</h1>
